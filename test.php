d71d68103f9e8cde0bf886c483de9a8086eb6c45
hosein <71982270+Dph107@users.noreply.github.com>
2021-01-03 21:44:24 +0330

    update test.php

--- test.php
+++ test.php
@@ -1,1 +1,23 @@
+<?php
+    
+require_once 'functions.php';
 
+if(!checkRemoteIPisTelegram())
+    die('You are not Telegram');
+
+const TOKEN = "xxx"; // API_KEY
+
+$update_array = json_decode(file_get_contents("php://input"), 1);  // JSON
+
+if (isset($update_array["message"])) {
+    $text    = $update_array["message"]["text"]; //text User
+    $chat_id = $update_array["message"]["chat"]["id"]; //chat id type
+
+if ($text == "/start") {
+    sendMessage([
+        'chat_id' => $chat_id,
+        'text' => var_export($update_array, true)
+    ]);
+}
+
+?>
